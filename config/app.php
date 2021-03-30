f0b3fe61511ede03ae3cbb17dfc09fe78c360018
Abdul Hadi Butt <hadibutt476@gmail.com>
2021-03-30 21:21:35 +0500

    Added Maatwebsite Excel

--- config/app.php
+++ config/app.php
@@ -175,6 +175,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Harimayco\Menu\MenuServiceProvider::class,
+        Maatwebsite\Excel\ExcelServiceProvider::class,
 
     ],
 
@@ -228,6 +229,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Menu' => Harimayco\Menu\Facades\Menu::class,
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
 
     ],
 
